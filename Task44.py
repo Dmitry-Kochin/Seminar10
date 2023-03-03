2f00d300bb4118ff7007ca75ce39f03784495fb3
Dmitry-Kochin <118636074+Dmitry-Kochin@users.noreply.github.com>
2023-03-03 16:15:01 +0300

    New file has been added

---
+++ Task44.py
@@ -0,0 +1,11 @@
+import pandas as pd
+from sklearn.preprocessing import LabelBinarizer
+lst = ['robot'] * 10
+lst += ['human'] * 10
+import random
+random.shuffle(lst)
+data = pd.DataFrame({'whoAmI':lst})
+y = LabelBinarizer().fit_transform(data)
+
+print(data.head())
+
